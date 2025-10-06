4f9e5059ac46749b3b11b8b84dd0434ad6021e32
Валерия Пономарева <ponomarevavaleria86@gmail.com>
2025-10-06 23:34:40 +0300

    Add files via upload

---
+++ homework.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+	setlocale(LC_ALL, "RUS");
+	int quantity;
+	float price_unit, price;
+
+	printf("Введите количество товара: ");
+	scanf("%d", &quantity);
+
+	printf("Введите стоимость товара за единицу: ");
+	scanf("%f", &price_unit);
+
+	price = quantity * price_unit;
+
+	printf("Общая стоимость покупки: %.2f", price);
+}
